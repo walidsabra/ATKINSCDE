113541ea3dd1cecce55090b795c99c4a0fd0e72c
walidsabra <walidsabra@softcut.net>
2016-09-23 08:26:58 +0400

    commit3

--- Testing App/Program.cs
+++ Testing App/Program.cs
@@ -43,12 +43,20 @@ namespace Testing_App
             int plAttributeId);
 
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct AADOC_ITEM
-        {
-            public int lProjectId;
-            public int lDocumentId;
-        }
+
+
+        
+        [DllImport("c:\\Program Files\\Bentley\\ProjectWise\\bin\\dmscli.dll", CharSet = CharSet.Unicode)]
+        public static extern int aaApi_SelectDocumentsByProjectId(int lProjectId);
+
+                
+        [DllImport("c:\\Program Files\\Bentley\\ProjectWise\\bin\\dmscli.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr aaApi_GetDocumentStringProperty(int lPropertyId, int index);
+
+        [DllImport("c:\\Program Files\\Bentley\\ProjectWise\\bin\\dmscli.dll", CharSet = CharSet.Unicode)]
+        private static extern int aaApi_GetDocumentNumericProperty(int lPropertyId, int index);
+
+
 
 
         static void Main(string[] args)
@@ -64,31 +72,75 @@ namespace Testing_App
             //bool Test = CreatePWFile();
 
             //Login to ProjectWise
-            bool test2 = aaApi_Initialize(0);
-            bool test3 = aaApi_Login(0, "DCH CDE", "admin", "admin", null);
+            bool Initialized = aaApi_Initialize(0);
+            bool validLogin = aaApi_Login(0, "DCH CDE", "admin", "admin", null);
 
-            Console.WriteLine(test2);
-            Console.WriteLine(test3);
+            Console.WriteLine("Initialized:" + Initialized);
+            Console.WriteLine("validLogin" + validLogin);
+
+            //Select Folder By Name
+
+            //Get Folder Id
+            //use her the API to get the ID
+            int PWFolderId = 282;
+
+            // Msppings
 
             string filepath = "D:\\PWTEST";
             DirectoryInfo d = new DirectoryInfo(filepath);
 
-            //Loop on file in Folder 
+            //Select files (all general attributes) within PW Folder
+            int selected = aaApi_SelectDocumentsByProjectId(PWFolderId);
+            Console.WriteLine("Selected?: " + selected);
+
+            //Loop on file in Staging Folder 
             foreach (var file in d.GetFiles("*.docx"))
             {
                 String ofilePath = file.FullName;
                 String ofileName = file.Name;
                 String oName = Path.GetFileNameWithoutExtension(file.FullName);
 
+
                 //if File already Exist on ProjectWise Folder - Create Version
 
+                for (int i = 0; i < selected; i++)
+                {
+                    string documentFileName = Marshal.PtrToStringUni(aaApi_GetDocumentStringProperty(21, i));
+                    int docSequence = aaApi_GetDocumentNumericProperty(2, i);
+                    string docVersion = Marshal.PtrToStringUni(aaApi_GetDocumentStringProperty(23, i));
 
-                //Else - Create New 
-                bool test4 = aaApi_CreateDocument(0, 282, 0, 0, 0, 0, 0, 0, ofilePath, ofileName, oName, null, null, false, 0, null, 1000, 0);
-                //bool test4 = aaApi_CreateDocument(0, 282, 0, 0, 0, 0, 0, 0, "D:\\PWTEST\\PWSDK.docx", "PWSDK.docx", "Test 4 from App", null, null, false, 0, null, 1000, 0);
-                Console.WriteLine(test4);
+                    if (documentFileName == ofileName)
+                    {
+                        Console.WriteLine("File: " + documentFileName +" Sequence: " + docSequence + " Version: " + docVersion+ " Already Exists in ProjectWise");
+                        //Change PW file State to Archived
 
-                //Delete File/s from Folder in here or outside the loop
+                        //creat new Revision here
+                    }
+                    else
+                    {
+                        //Else - Create New 
+                        bool test4 = aaApi_CreateDocument(0, PWFolderId, 0, 0, 0, 0, 0, 0, ofilePath, ofileName, oName, null, null, false, 0, null, 1000, 0);
+                        //bool test4 = aaApi_CreateDocument(0, 282, 0, 0, 0, 0, 0, 0, "D:\\PWTEST\\PWSDK.docx", "PWSDK.docx", "Test 4 from App", null, null, false, 0, null, 1000, 0);
+                        if (test4)
+                        {
+                            Console.WriteLine(ofileName + " Created Sucessfully on ProjectWise");
+
+                        }
+    
+                    }
+                    //change WF state to Shared
+
+                    // copy to P drive: - Overright
+
+                    // Notify Users 
+
+                    //Delete/Move File/s from Folder in here or outside the loop
+                }
+
+
+                
+
+               
             }
 
             
---
+++ Testing App/backupCode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_App
+{
+
+    class backupCode
+    {
+        [DllImport("c:\\Program Files\\Bentley\\ProjectWise\\bin\\dmscli.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int aaApi_SelectDocuments(IntPtr lpSelectInfo, IntPtr fpCallback, int lUserData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct AADOC_ITEM
+        {
+            public int lProjectId;
+            public int lDocumentId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct AADOCSELECT_ITEM
+        {
+            public uint ulFlags;
+            //Specifies flags for the document select operation.
+            public int lEnvironmentId;
+            //Specifies the environment identifier of the document item.
+            public int lProjectId;
+            //Specifies the project identifier of the document item.
+            public int lDocumentId;
+            //Specifies the unique document identifier.
+            public int lSetId;
+            //Specifies the set identifier of the document item.
+            public int lSetType;
+            //Specifies the set type of the document item.
+            public string lpctstrFileName;
+            //Pointer to a null-terminated string specifying the document file name.
+            public string lpctstrName;
+            //Pointer to a null-terminated string specifying the document name.
+            public string lpctstrDescription;
+            //Pointer to a null-terminated string specifying the document description.
+            public string lpctstrVersion;
+            //Pointer to a null-terminated string specifying the document version string.
+            public int lVersionSeq;
+            //Specifies the version sequence of the document item.
+            public int lOriginal;
+            //Specifies the original number of the document item.
+            public int lCreatorId;
+            //Specifies the creator's user identifier.
+            public int lUpdaterId;
+            //Specifies the last updater user identifier.
+            public int lLastUserId;
+            //Specifies the identifier of the user who has currently checked out the document or if the document is not checked out this member specifies the identifier of the user who was the last one that had the document checked out.
+            public string lpctstrStatus;
+            //Pointer to a null-terminated string specifying the document status.
+            public int lFileType;
+            //Specifies the file type of the document.
+            public int lItemType;
+            //Specifies the item type of the document.
+            public int lStorageId;
+            //Specifies the document file's storage identifier.
+            public int lWorkflowId;
+            ////Specifies the identifier of a workflow associated with the document.
+            public int lStateId;
+            ////Specifies the document's state identifier.
+            public int lApplicationId;
+            ////Specifies the identifier of an application assigned to the document.
+            public int lDepartmentId;
+            ////Specifies the identifier of the department assigned to the document.
+            public int lManagerId;
+            public int lItemFlags;
+            public int lManagerType;
+            public IntPtr lpProject;
+            public int lFileUpdator;
+
+        }
+
+        IntPtr AADMSBUFFER_DOCUMENT = IntPtr.Zero;
+
+        public static int SelectDocuments(AADOCSELECT_ITEM lpSelectInfo)
+        {
+            try
+            {
+                IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(lpSelectInfo));
+                Marshal.StructureToPtr(lpSelectInfo, ptr, true);
+                return aaApi_SelectDocuments(ptr, IntPtr.Zero, 0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
